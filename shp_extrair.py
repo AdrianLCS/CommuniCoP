fc893a61ce4384031cca31b53452beb1c28ec94d
AdrianLCS <adrian.lima.02.01@gmail.com>
2024-09-17 08:20:47 -0300

    ajustes na interface

--- shp_extrair.py
+++ shp_extrair.py
@@ -3,9 +3,9 @@ from shapely.geometry import shape, box
 
 # Defina a área de interesse (bounding box) - você pode ajustar essas coordenadas
 
-p1 = (-43.171140, -22.954110)
+"""p1 = (-43.171140, -22.954110)
 p2 = (-43.169465, -22.953343)
-
+"""
 def extrarir_do_rio(p1,p2):
     minx, miny = min(p1[0], p2[0])-1/360, min(p1[1], p2[1])-1/360
     maxx, maxy = max(p1[0], p2[0])+1/360, max(p1[1], p2[1])+1/360
