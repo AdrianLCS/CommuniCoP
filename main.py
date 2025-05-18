392fdb84f2937fbe3506be52e1106a1ad6685a81
AdrianLCS <114261968+AdrianLCS@users.noreply.github.com>
2025-05-18 17:35:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -206,7 +206,7 @@ def gerar_paths_alternativos(caminho_raster):
     nome_landcover = f"{lat_hem}{lat_val_ajustado:02d}{lon_hem}{lon_val_ajustado:03d}.tif"
 
     # Gera caminhos finais
-    caminho_dsm = os.path.join("dsm", nome_arquivo)
+    caminho_dsm = os.path.join("DSM", nome_arquivo)
     caminho_landcover = os.path.join("LandCover", nome_landcover)
 
     return caminho_dsm, caminho_landcover
@@ -271,7 +271,6 @@ def extrair_vet_area(raio, ponto, f, limear, unidade_distancia, precisao, local_
                 inv_transform_landcover[dema] = inv_transform_landcover_nd
 
     for i in range(int(360 * precisao)):
-        print(i)
         vet = np.array([np.cos(i * 2 * np.pi / qtd_retas), np.sin(
             i * 2 * np.pi / qtd_retas)])  # roda no sentido positivo trigonométrio de 2 em 2 graus
         pf = np.array(ponto) + vet * (
